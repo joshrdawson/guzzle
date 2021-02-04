6b86a0759a5b431810e317e144869d4e702a1238
Josh Dawson <joshuadawson123@googlemail.com>
2021-02-04 19:49:36 +0000

    init

---
+++ guzzle/guzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  guzzleApp.swift
+//  guzzle
+//
+//  Created by Josh on 04/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct guzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
