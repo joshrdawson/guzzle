dc47a4f2589229b7e0e5bc19cb9e8e70c63ca0db
Josh Dawson <joshuadawson123@googlemail.com>
2021-02-12 16:38:00 +0000

    re-made navigation bar

--- guzzle/ContentView.swift
+++ guzzle/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct ContentView: View {
 	@StateObject var viewRouter: ViewRouter
 	var body: some View {
-		GeometryReader { geometry in
 			VStack {
 				Spacer()
 				switch viewRouter.currentTab {
@@ -22,12 +21,23 @@ struct ContentView: View {
 					Text("Awards")
 				case .settings:
 					Text("Settings")
-				} 
+				}
+				
 				Spacer()
+				
 				HStack {
-					TabBar(viewRouter: viewRouter, assignedPage: .home, width: 90, height: 35, systemIcon: "house", tabName: "Home")
 					
-					TabBar(viewRouter: viewRouter, assignedPage: .me, width: 90, height: 35, systemIcon: "person", tabName: "Me")
+					HStack {
+						Spacer()
+						
+						BarItem(viewRouter: viewRouter, assignedPage: .home, systemIcon: "house", tabName: "Home")
+						
+						Spacer()
+						
+						BarItem(viewRouter: viewRouter, assignedPage: .me, systemIcon: "person", tabName: "Me")
+						
+						Spacer()
+					}
 					
 					ZStack {
 						Image(systemName: "plus.circle.fill")
@@ -37,15 +47,22 @@ struct ContentView: View {
 							.foregroundColor(Color.blue)
 					}.offset(y: -60)
 					
-					TabBar(viewRouter: viewRouter, assignedPage: .awards, width: 90, height: 35, systemIcon: "rosette", tabName: "Awards")
+					HStack {
+						Spacer()
+						
+						BarItem(viewRouter: viewRouter, assignedPage: .awards, systemIcon: "rosette", tabName: "Awards")
+						
+						Spacer()
+						
+						BarItem(viewRouter: viewRouter, assignedPage: .settings, systemIcon: "gear", tabName: "Settings")
+						
+						Spacer()
+					}
 					
-					TabBar(viewRouter: viewRouter, assignedPage: .settings, width: 90, height: 35, systemIcon: "gear", tabName: "Settings")
 				}
-				.frame(width: geometry.size.width, height: geometry.size.height/8)
 				.background(Color.black.opacity(0.05)).shadow(radius: 1)
 			}
 			.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-		}
 	}
 }
 
@@ -67,22 +84,22 @@ struct BlueView: View {
 	}
 }
 
-struct TabBar: View {
+struct BarItem: View {
 	@StateObject var viewRouter: ViewRouter
 	let assignedPage: Page
-	let width, height: CGFloat
 	let systemIcon, tabName: String
+	
 	var body: some View {
 		VStack {
-			Image(systemName: systemIcon).resizable().aspectRatio(contentMode: .fit)
-				.frame(width: width, height: height)
-				.padding(.top, 10)
+			Image(systemName: systemIcon)
 			Text(tabName).font(.footnote)
-			Spacer()
 		}
-		.onTapGesture{
+		.font(.system(size: 37.5))
+		.padding(.top, 10)
+		.padding(.bottom, 30)
+		.onTapGesture {
 			viewRouter.currentTab = assignedPage
 		}
-		.padding(.horizontal, -4)
+		
 	}
 }
